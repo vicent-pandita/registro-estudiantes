a376a58519eb07945c158267c4fadb1ff7209800
Vicent Pandita <1006420231@unajma.edu.pe>
2025-12-09 22:14:57 -0500

    feat: Add course catalog modal, update README with deployment URL, and fix dashboard components

--- src/app/cursos/catalogo/catalogo.component.ts
+++ src/app/cursos/catalogo/catalogo.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class CatalogoComponent {
   nivelActivo: 'primaria' | 'secundaria' = 'primaria';
+
+  cursosOficiales = {
+    primaria: [
+      'Matemática',
+      'Comunicación (Castellano, Lenguas Originarias)',
+      'Ciencia y Tecnología',
+      'Personal Social',
+      'Arte y Cultura',
+      'Educación Física',
+      'Educación Religiosa',
+      'Tutoría y Orientación Educativa',
+      'Inglés como Lengua Extranjera'
+    ],
+    secundaria: [
+      'Matemática',
+      'Comunicación',
+      'Inglés',
+      'Arte y Cultura',
+      'Ciencias Sociales',
+      'Desarrollo Personal, Ciudadanía y Cívica',
+      'Educación Física',
+      'Educación Religiosa',
+      'Ciencia y Tecnología',
+      'Educación para el Trabajo (EPT)',
+      'Tutoría y Orientación Educativa'
+    ]
+  };
 }
